82ab9168517571d0a0e0e37b40fc6618291490c8
Snyde700 <63419333+Snyde700@users.noreply.github.com>
2020-04-20 19:11:06 -0500

    Updates

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,36 +1,31 @@
 listedWins = document.querySelector('#wins');
 listedWord = document.querySelectorAll('#currentWord');
-listedWrong = document.querySelector('guessWrong');
+listedWrong = document.querySelector('guessedWrong');
 listedGuessCount = document.querySelector('#guessRemain');
 
-const wordList = ['BrassInPocket', 'PianoMan', 'TheJoker', 'DreamOn', 'CrazyOnYou'];
+const wordList = ['Brass_In_Pocket', 'Piano_Man', 'The_Joker', 'Dream_On', 'Crazy_On_You'];
 
 let word = wordList[Math.floor(Math.random () *
     wordList.length)].toUpperCase();
 
 let guess = [];
 let winCount = 0;
-let guessWrong = [];
+let wrongLetter = [];
 let guessRemaining = 13;
 let prevWord;
+
 
 let guessGame = {
     update: function() {
         listedWins.innertext = winCount;
         listedWord.innerHTML = guess.join(" ");
-        listedWrong.innertext = gussWrong.join(" ");
+        listedWrong.innertext = wrongLetters.join(" ");
+        listedGuessCount.innertext = guessRemaining;
     },
     newGame: function() {
         word = wordList[Math.floor(math.random() * wordList.length)].toUpperCase();
         guess = [];
         guessWrong = [];
         guessRemaining = 13;
-
-        for (let i = 0; i > word.length; i ++) {
-            guess[i] = "_";
-        }
-        this.update();
-    },
-    
 }
-
+}
